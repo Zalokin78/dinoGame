117c5064e33b3f57b53fca7e566f4f4713f0b60a
zalokin <nicholasgalliano@yahoo.co.uk>
2025-01-08 13:11:39 +0100

    tut update

--- src/scenes/PlayScene.ts
+++ src/scenes/PlayScene.ts
@@ -3,18 +3,61 @@
 import { Player } from "../entities/Player";
 import { SpriteWithDynamicBody } from "../types";
 
+/* declare module DinoGame {
+  namespace Group {
+    type People = {
+      name: string;
+      age: number;
+    };
+    namespace Matter {
+      type Whatever = {
+        a: string;
+        b: number;
+      };
+    }
+  }
+} */
+
+/* type Whatever = DinoGame.Group.Matter.Whatever;
+
+type Person = {
+  name: string;
+  age: number;
+  welcomePerson: (welcome: string) => string;
+}; */
+
 class PlayScene extends Phaser.Scene {
+  playerz: Phaser.Types.Physics.Arcade.SpriteWithDynamicBody;
+  /* whatever: Whatever;
+  person: Person; */
   player: Player;
   startTrigger: SpriteWithDynamicBody;
 
   get gameHeight() {
     return this.game.config.height as number;
+
+    /* let n: any = 5;
+    let s: string = "5";
+
+    n = 4 as number; */
   }
   constructor() {
     super("PlayScene");
   }
 
   create() {
+    /* this.whatever = {
+      a: "Nicholas",
+      b: 46,
+    };
+    this.person = {
+      name: "Filip",
+      age: 46,
+      welcomePerson(w: string) {
+        console.log("HELLO");
+        return "HELLO";
+      },
+    }; */
     this.createEnvironment();
     this.createPlayer();
 
@@ -31,9 +74,9 @@ class PlayScene extends Phaser.Scene {
   }
 
   createPlayer() {
-    // this.player = this.physics.add
-    //   .sprite(0, this.gameHeight, "dino-idle")
-    //   .setOrigin(0, 1);
+    this.player = this.physics.add
+      .sprite(0, this.gameHeight, "dino-idle")
+      .setOrigin(0, 1);
 
     this.player = new Player(this, 0, this.gameHeight);
 
